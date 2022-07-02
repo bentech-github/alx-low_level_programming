7c092531dcc0d167f75da4b035cdb2b1066b061a
“Ben” <bensonadedayo123@gmail.com>
2022-07-02 08:03:06 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - this is the main of the function
- * description - this is the start of the function
-
+ * return - this is the start of the function
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
