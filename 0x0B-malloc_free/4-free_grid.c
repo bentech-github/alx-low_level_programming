794e81e9639136de37dc22b4d16b63b606dd132c
“Ben” <bensonadedayo123@gmail.com>
2022-07-27 18:13:04 -0700

    This is memory allocation

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - free a 2 dimensional grid
+ * @grid: grid
+ * @height: int
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int a;
+
+	a = height - 1;
+
+	while (a >= 0)
+	{
+		free(grid[a]);
+
+		grid[a] = NULL;
+
+		a--;
+	}
+	free(grid);
+
+	grid = NULL;
+}
